e07bbd8ec80fc43a8a95a2b66a44c3f849eff9b4
поскребышева полина <example@ex.com>
2026-03-03 20:35:34 +0500

    добавлен ридми файл

--- Math_Library/obj/Debug/net8.0/Math_Library.AssemblyInfo.cs
+++ Math_Library/obj/Debug/net8.0/Math_Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Math_Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c30c40250870f1082ebf7997387467f7a4ba152")]
 [assembly: System.Reflection.AssemblyProductAttribute("Math_Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Math_Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
